7216bca0393ef26e8f306a66e36cb2ec04b9ffef
Nalooto <973022448@qq.com>
2026-06-23 14:05:50 +0800

    fix(Shikigami):概率卡寄养未点击确定

--- tasks/Component/ReplaceShikigami/replace_shikigami.py
+++ tasks/Component/ReplaceShikigami/replace_shikigami.py
@@ -102,15 +102,24 @@ class ReplaceShikigami(BaseTask, ReplaceShikigamiAssets):
             # 恢复点击操作
             if click_interval_timer.reached_and_reset():
                 clicked = False
-            
-            self.screenshot()
 
-            if not self.appear(stop_image):
-                break
+            self.device.stuck_record_clear()
+            self.screenshot()
 
             if self.appear_then_click(self.I_U_CONFIRM_SMALL, interval=0.5):
                 clicked = False  # 点击了确认, 恢复选式神的操作
                 continue
+
+            if self.appear_then_click(self.I_U_CIRCLE_ALTERNATE, interval=2.5):
+                self.appear_then_click(self.I_U_CONFIRM_ALTERNATE, interval=1.5)
+                continue
+
+            if stop_image is None:
+                logger.warning('Set shikigami called without stop_image, exit')
+                return
+
+            if not self.appear(stop_image):
+                break
 
             # 与下方点击第7个式神操作互斥, 防止确认按钮还没有出现被下方取消掉
             if not clicked and self.click(click_match, interval=1.5):
@@ -121,9 +130,7 @@ class ReplaceShikigami(BaseTask, ReplaceShikigamiAssets):
                 # 导致一直无法选上
                 clicked = True
                 continue
-            if self.appear_then_click(self.I_U_CIRCLE_ALTERNATE, interval=2.5):
-                self.appear_then_click(self.I_U_CONFIRM_ALTERNATE, interval=1.5)
-                continue
+            time.sleep(0.2)
         logger.info('Set shikigami: %d' % shikigami_order)
 
     def detect_no_shikigami(self) -> bool:
